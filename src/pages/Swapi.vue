9e484fb66fc5b35421b40d2e61ce718d096aaaf0
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2021-04-13 21:26:27 +0200

    fetch and display data

--- src/pages/Swapi.vue
+++ src/pages/Swapi.vue
@@ -1,16 +1,63 @@
 <template>
   <Layout>
-    <h1>Swapi</h1>
-    <p>
-      here content
-    </p>
+    <div>
+      <h1>Star Wars API</h1>
+      <table
+        class="flex flex-col justify-items-center place-items-center bg-gray-300"
+      >
+        <thead class="bg-gray-500 w-full text-center ">
+          <tr class="flex justify-items-center place-items-center">
+            <th class="w-1/5">Name</th>
+            <th class="w-1/5">Eyes Color</th>
+            <th class="w-1/5">Gender</th>
+            <th class="w-1/5">Hair Color</th>
+            <th class="w-1/5">Height</th>
+            <th class="w-1/5">Mass</th>
+          </tr>
+        </thead>
+        <tbody class="w-full text-center">
+          <tr v-for="character in allCharacters" :key="character.name">
+            <td class="w-1/5">{{ character.name }}</td>
+            <td class="w-1/5">{{ character.eye_color }}</td>
+            <td class="w-1/5">{{ character.gender }}</td>
+            <td class="w-1/5">{{ character.hair_color }}</td>
+            <td class="w-1/5">{{ character.mass }}</td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
   </Layout>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   metaInfo: {
     title: "SWAPI",
   },
-}
+  data() {
+    return {
+      baseURL: "https://swapi.dev/api",
+      allCharacters: [],
+    };
+  },
+  methods: {
+    async getAllCharacters() {
+      let counter = 1;
+      let response = null;
+      do {
+        response = (await axios.get(
+          `${this.baseURL}/people/?page=${counter++}`
+        )).data;
+        console.log(response);
+
+        this.allCharacters.push(...response.results);
+      } while (response.next);
+    },
+  },
+  beforeMount() {
+    this.getAllCharacters();
+  },
+};
 </script>
