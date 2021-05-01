1a7087103a51b57d121e93f9adf555d80af704d9
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2021-05-01 21:17:12 +0200

    better styles for pokemons

--- src/pages/Pokemons.vue
+++ src/pages/Pokemons.vue
@@ -1,31 +1,40 @@
 <template>
   <Layout>
     <div>
-      <h1>Pokemon API</h1>
-      <p>Select pokemon to retrieve info about'em</p>
-      <Loader v-if="isLoaderActive" />
-      <div v-else class="flex">
-        <ul class="flex flex-wrap space-x-1 w-8/12">
-          <li v-for="pokemon in pokemons" :key="pokemon.name">
-            <button @click="getSpecificPokemon(pokemon.name)">
-              {{ pokemon.name }}
-            </button>
-          </li>
-        </ul>
-
-        <div class="bg-gray-400" v-if="actualPokemon">
+      <header class="flex justify-evenly place-items-center bg-gray-300">
+        <div class=" space-y-2 p-4">
+          <h1 class="p-2">Pokemon API</h1>
+          <p class="p-2">Select pokemon to retrieve info about'em</p>
+        </div>
+        <div class="p-4" v-if="actualPokemon">
           <p>{{ actualPokemon.name }}</p>
           <div class="flex w-60">
             <g-image
-              class="w-full"
+              class="h-36"
               :src="actualPokemon.sprites.front_default"
             ></g-image>
-            <g-image :src="actualPokemon.sprites.back_default"></g-image>
+            <g-image
+              class="h-36"
+              :src="actualPokemon.sprites.back_default"
+            ></g-image>
           </div>
           <p v-for="(type, index) in actualPokemon.types" :key="index">
             Type: {{ type.type.name }}
           </p>
         </div>
+      </header>
+      <Loader v-if="isLoaderActive" />
+      <div v-else class="flex justify-center py-4">
+        <ul class="flex flex-wrap space-x-1 w-8/12">
+          <li v-for="pokemon in pokemons" :key="pokemon.name">
+            <button
+              class="shadow-lg p-2"
+              @click="getSpecificPokemon(pokemon.name)"
+            >
+              {{ pokemon.name }}
+            </button>
+          </li>
+        </ul>
       </div>
     </div>
   </Layout>
