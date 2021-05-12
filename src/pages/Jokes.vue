d31d4b2d7c4360d1df348280e419fd1be5b9ea9c
Marcin Spławiński <marcin.cincin9010@gmail.com>
2021-05-12 12:44:57 +0200

    working jokes generator

--- src/pages/Jokes.vue
+++ src/pages/Jokes.vue
@@ -6,13 +6,24 @@
       </div>
     </template>
     <template #aside>
-      <div class="bg-red-50 w-full lg:w-1/4 p-8">Aside</div>
+      <div class="bg-red-50 w-full lg:w-1/4 p-8">
+        <button @click="getSingleJoke" class="bg-red-800">Let's roll</button>
+      </div>
     </template>
     <div class="bg-red-400 w-full lg:w-3/4 p-8">
-      tutaj właściwy kontent joków
+      <h1>Random Joke generator</h1>
+      <Loader v-if="isLoaderActive" />
+      <p v-else>
+        Actual joke:
+        <strong v-if="actualJoke.setup">
+          {{ actualJoke.setup }}
+          {{ actualJoke.delivery }}
+        </strong>
+        <strong v-else>{{ actualJoke.joke }}</strong>
+      </p>
     </div>
     <template #footer>
-      footer
+      <p>You read: {{ jokesCount }} jokes</p>
     </template>
   </JokesLayout>
 </template>
@@ -30,9 +41,34 @@ export default {
     JokesLayout,
   },
   data() {
-    return {}
+    return {
+      jokeAPIBaseURL: "https://v2.jokeapi.dev/joke/Any",
+      actualJoke: {},
+      isLoaderActive: false,
+      jokesCount: 0,
+    }
   },
-  methods: {},
+  methods: {
+    async getSingleJoke() {
+      this.isLoaderActive = true
+      await axios.get(this.jokeAPIBaseURL).then((res) => {
+        if (res.data.setup) {
+          const { setup, delivery } = res.data
+          //   console.log(setup, delivery)
+          this.actualJoke = { setup, delivery }
+        } else {
+          const { joke } = res.data
+          //   console.log(joke)
+          this.actualJoke = { joke }
+        }
+      })
+      this.isLoaderActive = !this.isLoaderActive
+      this.jokesCount++
+    },
+  },
+  beforeMount() {
+    this.getSingleJoke()
+  },
 }
 </script>
 
