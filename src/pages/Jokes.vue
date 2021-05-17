2ef82aff98fe1c1615285e2b7cfe4a321e65e521
Marcin Spławiński <64729676+yakuza16@users.noreply.github.com>
2021-05-17 19:29:20 +0200

    styled jokes page

--- src/pages/Jokes.vue
+++ src/pages/Jokes.vue
@@ -6,33 +6,48 @@
       </div>
     </template>
     <template #aside>
-      <div class="bg-red-50 w-full lg:w-1/4 p-8">
-        <button @click="getSingleJoke" class="bg-red-800">Let's roll</button>
+      <div class=" w-full lg:w-1/4 p-8">
+        <button
+          @click="getSingleJoke"
+          class="p-4 shadow-xl rounded-lg border-green-900 border-4 focus:outline-none focus:ring-2 xl:text-2xl font-extrabold transition duration-500 ease-in-out bg-white text-green-900 hover:text-green-400 hover:bg-green-900 hover:shadow-2xl hover:border-green-200"
+        >
+          Draw joke
+        </button>
       </div>
     </template>
-    <div class="bg-red-400 w-full lg:w-3/4 p-8">
-      <h1>Random Joke generator</h1>
+    <div class="w-full lg:w-3/4 p-8 xl:h-96">
+      <h1 class="font-extrabold my-2 xl:text-4xl">Random Joke generator</h1>
       <Loader v-if="isLoaderActive" />
-      <p v-else>
+      <p class="my-6" v-else>
         Actual joke:
-        <strong v-if="actualJoke.setup">
+        <strong class="block my-6 xl:text-2xl" v-if="actualJoke.setup">
           {{ actualJoke.setup }}
           {{ actualJoke.delivery }}
         </strong>
-        <strong v-else>{{ actualJoke.joke }}</strong>
+        <strong class="block my-6 xl:text-2xl" v-else>{{
+          actualJoke.joke
+        }}</strong>
       </p>
     </div>
     <template #footer>
-      <p>You read: {{ jokesCount }} jokes</p>
+      <p class="text-center pb-12 xl:text-2xl">
+        You read:
+        <span
+          class="font-extrabold"
+          :class="[jokesCount % 2 ? 'text-white' : 'text-black']"
+          >{{ jokesCount }}</span
+        >
+        joke<span v-if="jokesCount > 1">s</span>
+      </p>
     </template>
   </JokesLayout>
 </template>
 
 <script>
-import axios from "axios"
-import Loader from "../components/Loader"
-import Navigation from "../components/Navigation"
-import JokesLayout from "../layouts/JokesLayout"
+import axios from "axios";
+import Loader from "../components/Loader";
+import Navigation from "../components/Navigation";
+import JokesLayout from "../layouts/JokesLayout";
 
 export default {
   components: {
@@ -46,30 +61,30 @@ export default {
       actualJoke: {},
       isLoaderActive: false,
       jokesCount: 0,
-    }
+    };
   },
   methods: {
     async getSingleJoke() {
-      this.isLoaderActive = true
+      this.isLoaderActive = true;
       await axios.get(this.jokeAPIBaseURL).then((res) => {
         if (res.data.setup) {
-          const { setup, delivery } = res.data
+          const { setup, delivery } = res.data;
           //   console.log(setup, delivery)
-          this.actualJoke = { setup, delivery }
+          this.actualJoke = { setup, delivery };
         } else {
-          const { joke } = res.data
+          const { joke } = res.data;
           //   console.log(joke)
-          this.actualJoke = { joke }
+          this.actualJoke = { joke };
         }
-      })
-      this.isLoaderActive = !this.isLoaderActive
-      this.jokesCount++
+      });
+      this.isLoaderActive = !this.isLoaderActive;
+      this.jokesCount++;
     },
   },
   beforeMount() {
-    this.getSingleJoke()
+    this.getSingleJoke();
   },
-}
+};
 </script>
 
 <style></style>
